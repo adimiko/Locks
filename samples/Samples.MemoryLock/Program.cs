44f6e62d1c7514196aa706f90c275c1403882442
Adrian Mikołajczyk <53557466+adimiko@users.noreply.github.com>
2024-07-11 13:05:11 +0200

    Added sample (#2)

---
+++ samples/Samples.MemoryLock/Program.cs
@@ -0,0 +1,19 @@
+﻿using Locks;
+using Microsoft.Extensions.DependencyInjection;
+
+
+IServiceCollection services = new ServiceCollection();
+
+services.AddMemoryLock();
+
+var sp = services.BuildServiceProvider();
+
+using (var scope = sp.CreateScope())
+{
+    var memoryLock = scope.ServiceProvider.GetRequiredService<IMemoryLock>();
+
+    using (memoryLock.Acquire("YOUR_KEY"))
+    {
+        Console.WriteLine("YOUR_LOGIC");
+    }
+}
